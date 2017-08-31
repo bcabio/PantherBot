af3ef4ff4e9c6e30cbf9fe60f9ae21d5a982cb43
Brian Cabigon-Soliber <brian.cabigonsoliber@ge.com>
2017-08-31 10:48:51 -0400

    command fix

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pep8count=`find . -name \*.py -exec pep8 --ignore=E402,E501 {} + | wc -l`
+
+if [ $pep8count != 0 ];
+	then
+    	find . -name \*.py -exec pep8 --ignore=E402,E501 {} + 
+   		exit 1
+    else
+        echo 'All good'
+        exit 0
+    fi
